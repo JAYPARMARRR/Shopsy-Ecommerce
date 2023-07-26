1c2ab8dfd89f0d8c43555eccb22ede156d444dd7
Parmar Jay <jaykumarvinodbhi@gmail.com>
2023-07-26 08:16:38 +0530

    add new fi,

--- src/Componentes/item.jsx
+++ src/Componentes/item.jsx
@@ -1,23 +1,14 @@
-import { useLocation } from "react-router-dom";
-import React, { useEffect } from "react";
+// import { useLocation } from "react-router-dom";
+// import React, { useEffect } from "react";
 import "../Componentes/Item.css";
 import jayu from "../Componentes/img/2.jpg";
 const Item = () => {
+
+
+
   // const location = useLocation();
   // console.log(location);
 
-  // useEffect(() => {
-  //   data2();
-  // }, []);
-
-  // const data2 = async () => {
-  //   try {
-  //     const rep2 = await axios.get("https://dummyjson.com/products/1");
-  //     console.log(">>>>>>>>>>>", rep2);
-  //   } catch (error) {
-  //     console.error("Error>>>>>>>>>>>>>>>>", error);
-  //   }
-  // };
 
   return (
     <div>
